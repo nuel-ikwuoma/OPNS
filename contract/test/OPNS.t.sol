e12a1cdee84b63c6326a4050d8b349f33bd3968f
nuel-ikwuoma <emmanuelikwuoma7@gmail.com>
2023-08-31 14:28:21 +0100

    init commit

---
+++ contract/test/OPNS.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/OPNS.sol";
+
+contract CounterTest is Test {
+    OPNS public opns;
+
+    function setUp() public {
+        opns = new OPNS();
+    }
+
+    function testGatwayAddress() public {
+        assert(true);
+    }
+
+}
